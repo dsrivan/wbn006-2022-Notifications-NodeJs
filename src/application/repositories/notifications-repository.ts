194aff211a803b258b66c60d0493f6939e8d8cd0
Ivan da Silva Rosa <iivan.si@msn.com>
2022-12-17 23:17:40 -0300

    configs + add notification cancel feature

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -2,6 +2,8 @@ import { Notification } from "../entities/notification";
 
 abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
+  abstract save(notification: Notification): Promise<void>;
 }
 
 export default NotificationsRepository;
---
+++ src/application/use-cases/errors/notification-not-found.ts
@@ -0,0 +1,6 @@
+class NotificationNotFound extends Error {
+  constructor() {
+    super('Notification not found.');
+  };
+}
+export default NotificationNotFound;
